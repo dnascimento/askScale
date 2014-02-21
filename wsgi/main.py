9a29fd1a3e10d7ae31274c197383df874626dd0c
Dario Nascimento <dfrnascimento@gmail.com>
2014-02-21 21:24:48 +0100

    up

--- wsgi/main.py
+++ wsgi/main.py
@@ -1,4 +1,4 @@
-# from bottle import route, run, template, get, error, request, response, redirect, static_file, default_app
+from bottle import route, run, template, get, error, request, response, redirect, static_file, default_app
 # #import bottle
 
 # #Parts
@@ -6,93 +6,93 @@
 # from votes import *
 # from user import *
 
-# db = Storage()
+db = Storage()
 
 # application = bottle.Bottle()
 
-# @application.get("/")
-# def index():
-# 	request.headers.get('cookie')
-# 	response.add_header('RID', '124')
-# 	questionList = db.getQuestionList(20)
-# 	return template("index.tpl",name="dario",questionList=questionList)
+@get("/")
+def index():
+	request.headers.get('cookie')
+	response.add_header('RID', '124')
+	questionList = db.getQuestionList(20)
+	return template("index.tpl",name="dario",questionList=questionList)
 
 
-# @application.error(404)
-# def error404(error):
-#     return 'Nothing here, sorry'
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
 
 
-# @application.route('/static/<filepath:path>')
-# def server_static(filepath):
-# 	return static_file(filepath,root="./static")
+@route('/static/<filepath:path>')
+def server_static(filepath):
+	return static_file(filepath,root="./static")
 
 
 
 
-# ################ ANSWERS ##########################
+################ ANSWERS ##########################
 
-# @application.post("/question/<title>/answer")
-# def postAnswer(title):
-# 	author = ""
-# 	answer = request.forms.get('answer')
-# 	db.addAnswer(title,answer,author)
-# 	redirect("/question/"+title) 
+@post("/question/<title>/answer")
+def postAnswer(title):
+	author = ""
+	answer = request.forms.get('answer')
+	db.addAnswer(title,answer,author)
+	redirect("/question/"+title) 
 
-# ############ comments #########################
-# @application.post("/question/<title>/comment")
-# def postComment(title):
-# 	author = ""
-# 	ident = request.forms.get("ident")
-# 	answer = request.forms.get('answer')
-# 	comment = request.forms.get('comment')
-# 	db.addComment(ident,title,answer,comment,author)
-# 	redirect("/question/"+title) 
-
-
-# ########### questions ########################
-# @application.get("/new-question")
-# def getNewQuestion():
-# 	return  (template("newQuestion.tpl",tags=['nice','novo']))
-
-# @application.post("/new-question")
-# def postNewQuestion():
-# 	title = request.forms.get('title')
-# 	content = request.forms.get('content')
-# 	tags = request.forms.getlist('tags')
-# 	author = ""
-# 	db.addNewQuestion(title,content,tags,author)
-# 	redirect("/question/"+title)
-
-# @application.get("/question/<title>")
-# def getQuestion(title):
-# 	questionData = db.getQuestion(title)
-# 	return template('question.tpl',questionData=questionData)
-
-# ################# USER ########################
-# @application.get("/login")
-# def login():
-# 	if isLogin():
-# 		redirect("/")
-# 	else:
-# 		return template('login')
-
-# @application.get("/user/<username>")
-# def viewUser(username):
-# 	print "view"
+############ comments #########################
+@post("/question/<title>/comment")
+def postComment(title):
+	author = ""
+	ident = request.forms.get("ident")
+	answer = request.forms.get('answer')
+	comment = request.forms.get('comment')
+	db.addComment(ident,title,answer,comment,author)
+	redirect("/question/"+title) 
 
 
-# ################ VOTES ####################
-# @application.get("/question/<title>/<answer>/up")
-# def voteUp(title,answer):
-# 	votes = db.voteUp(title, answer)
-# 	redirect("/question/"+title)
+########### questions ########################
+@get("/new-question")
+def getNewQuestion():
+	return  (template("newQuestion.tpl",tags=['nice','novo']))
+
+@post("/new-question")
+def postNewQuestion():
+	title = request.forms.get('title')
+	content = request.forms.get('content')
+	tags = request.forms.getlist('tags')
+	author = ""
+	db.addNewQuestion(title,content,tags,author)
+	redirect("/question/"+title)
+
+@get("/question/<title>")
+def getQuestion(title):
+	questionData = db.getQuestion(title)
+	return template('question.tpl',questionData=questionData)
+
+################# USER ########################
+@get("/login")
+def login():
+	if isLogin():
+		redirect("/")
+	else:
+		return template('login')
+
+@get("/user/<username>")
+def viewUser(username):
+	print "view"
 
 
-# @application.get("/question/<title>/<answer>/down")
-# def voteDown(title,answer):
-# 	votes = db.voteDown(title, answer)
-# 	redirect("/question/"+title)
+################ VOTES ####################
+@get("/question/<title>/<answer>/up")
+def voteUp(title,answer):
+	votes = db.voteUp(title, answer)
+	redirect("/question/"+title)
+
+
+@get("/question/<title>/<answer>/down")
+def voteDown(title,answer):
+	votes = db.voteDown(title, answer)
+	redirect("/question/"+title)
 
 
 # ############################################################
@@ -104,24 +104,6 @@
 # #application.run(server='gae')
 
 # #Openshift
-# # This must be added in order to do correct path lookups for the views
-# import os
-# from bottle import TEMPLATE_PATH
-# TEMPLATE_PATH.applicationend(os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 
-#     'runtime/repo/wsgi/views/')) 
-
-# application=default_app()
-
-from bottle import route, default_app
-
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hello, %s!</strong>' % name
- 
-@route('/')
-def index():
-    return '<strong>Hello World!</strong>'
-
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
@@ -129,3 +111,8 @@ TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'],
     'runtime/repo/wsgi/views/')) 
 
 application=default_app()
+
+
+
+
+
