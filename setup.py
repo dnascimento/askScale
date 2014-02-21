358bdd128f84f702ca9f7529cd8a826a5388351b
Dario Nascimento <dfrnascimento@gmail.com>
2014-02-21 01:58:53 +0100

    openshift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Ask',
+      version='1.0',
+      description='OpenShift Ask',
+      author='Dario Nascimento',
+      author_email='dfrnascimento@gmail.com',
+      url='http://darionascimento.com',
+      install_requires=['bottle'],
+     )
